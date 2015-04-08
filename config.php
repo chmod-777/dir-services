64058bab18621529c03be4d6872c28c4aceb357a
chmod-777 <sales@webgems.info>
2015-04-08 09:03:31 +0200

    added db info to config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 	
 	$dbhost = '127.0.0.1';
-	$dbuser = 'root';
-	$dbpass = '';
-	$dbname = 'directory';
+	$dbuser = 'bloghaga_dir-app';
+	$dbpass = 'o^*?l~GgqHun';
+	$dbname = 'bloghaga_dir';
 
 ?>
